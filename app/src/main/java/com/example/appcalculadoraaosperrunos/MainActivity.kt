30a7b4c68c9ac8af4d3a023bbcb9b038219ae1c3
EdsonAleman910 <edsnalemancu@gmail.com>
2024-09-11 22:06:29 -0600

    Se cambio el Layout, añadiendo TextField y Boton.

--- app/src/main/java/com/example/appcalculadoraaosperrunos/MainActivity.kt
+++ app/src/main/java/com/example/appcalculadoraaosperrunos/MainActivity.kt
@@ -1,15 +1,32 @@
 package com.example.appcalculadoraaosperrunos
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.appcalculadoraaosperrunos.ui.theme.AppCalculadoraAñosPerrunosTheme
 
@@ -32,10 +49,31 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    val imagen = painterResource(id = R.drawable.perro)
+    var text by remember { mutableStateOf(TextFieldValue("")) }
+    Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Top){
+        Image(
+            painter = imagen,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.align(Alignment.CenterHorizontally).fillMaxWidth())
+        Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.SpaceAround) {
+            OutlinedTextField(
+                value = text,
+                onValueChange = { newText ->
+                    text = newText
+                },
+                label = {
+                    Text(text = "Ingresa la edad")
+                },
+                placeholder = { Text(text = "Ingresa tu edad")}
+            )
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "CALCULAR")
+            }
+        }
+
+    }
 }
 
 @Preview(showBackground = true)
